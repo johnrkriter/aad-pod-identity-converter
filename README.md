742ecd503570ad400cb382a877362f10517d55cb
john.r.kriter <john.r.kriter@accenture.com>
2020-08-27 18:57:26 +0800

    Added logging and initial yaml output for identity

--- README.md
+++ README.md
@@ -4,7 +4,12 @@
 # aad-pod-identity-converter
 Read in a list of azure managed identity and convert them to YAML for use with aad-pod-identity
 
-
+# Local environment setup
+standard python setup
+1. create a ```venv``` folder in your root locally.
+2. setup a local venv ```virtualenv -p `which python3` venv```
+3. activate local environment either in editor(like vsCode) or in terminal ```source venv/bin/activate```
+4. install your requirements ```pip install -r requirements.txt```
 
 # Additional Information
 For more information about about using Azure Identity on AKS, refer to [Azure/aad-pod-identity](https://github.com/Azure/aad-pod-identity)
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 #This program is designed to read in azure Managed Identity information, and produce YAML for aad-pod-identity usage
-import PyYaml
-import csv
 
-azuremanagedidentitylist = {}
+#Base Python Packages
+import csv
+import logging
+
+#from PyYAML
+import yaml
+
+#from logzero
+import logzero
+from logzero import logger
+
+##initialize logging
+logzero.loglevel(logging.DEBUG)
+
+#initialized dictionary containing dictionaries of MSI configuration information
+msilist = {}
 
 class AzureManagedIdentity:
     def ___init___(self, msi, clientId, objectId, resourceId):
@@ -18,20 +31,27 @@ def read_input_csv(filePath):
         line_count = 0
         for row in reader:
             if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
+                logger.debug(f'Column names are {", ".join(row)}')
                 line_count += 1
-            print(f'\t{row["msi"]} as msi: {row["Client ID"]} as Client ID: {row["Object ID"]} as Object ID: {row["Resource ID"]} as Resource ID.')
-            azuremanagedidentitylist[line_count]=row
+            logger.debug(f'\t{row["msi"]} as msi: {row["Client ID"]} as Client ID: {row["Object ID"]} as Object ID: {row["Resource ID"]} as Resource ID.')
+            msilist[line_count]=row
             line_count += 1
-    print("LINE BREAK")
-    print(azuremanagedidentitylist)
+    logger.debug(f'msi list: {msilist}')
     csvfile.close()
 
 def write_azure_identity(identityList):
     """Takes in a nested dictionary of identities and creates azure identity yaml for each"""
-
-
+    logger.debug(len(identityList))
+    for identity_id, identity_info in identityList.items():
+        logger.debug(f'processing msi #{identity_id} containing')
+        filename = identityList[identity_id]['msi'] + '.yaml'
+        resourceId = identityList[identity_id]['Resource ID']
+        with open (filename, 'w') as identityfile:
+            logger.debug(f'processing file:{filename}')
+            identityfile.write(f'example text: {resourceId}')
+            identityfile.close()
 
 #read_input("single-line.csv")
-read_input_csv("azure-identities.csv")
+read_input_csv("example-identities.csv")
+write_azure_identity(msilist)
 
