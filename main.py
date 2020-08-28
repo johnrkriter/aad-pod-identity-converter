a7bb741955266641048cb87b87e43dfe8b5c6aec
john.r.kriter <john.r.kriter@accenture.com>
2020-08-28 11:11:14 +0800

    Completed initial version

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 #This program is designed to read in azure Managed Identity information, and produce YAML for aad-pod-identity usage
 
 #Base Python Packages
-import csv
-import logging
-
-#from PyYAML
-import yaml
+import csv, logging, os, re
 
 #from logzero
 import logzero
@@ -16,13 +12,6 @@ logzero.loglevel(logging.DEBUG)
 
 #initialized dictionary containing dictionaries of MSI configuration information
 msilist = {}
-
-class AzureManagedIdentity:
-    def ___init___(self, msi, clientId, objectId, resourceId):
-        self.msi = msi
-        self.clientId = clientId
-        self.objectId = objectId
-        self.resourceId = resourceId
 
 def read_input_csv(filePath):
     """Reads in a CSV file and creates a dictionary of each line, adding it to a collection dictionary"""
@@ -46,12 +35,13 @@ def write_azure_identity(identityList):
         logger.debug(f'processing msi #{identity_id} containing')
 
         # Convert the data in the dictionary item into variables
-        filename = identity_info['msi'] + '.yaml'
+        filename = './output/' + identity_info['msi'] + '.yaml'
         identityName = identity_info['msi']
         resourceId = identity_info['Resource ID']
         clientId = identity_info['Client ID']
 
         #Create a single YAML for the AzureIdentity for each item in identityList
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
         with open (filename, 'w') as identityfile:
             logger.debug(f'processing file:{filename}')
             identityfile.write(f'apiVersion: "aadpodidentity.k8s.io/v1"\n')
@@ -72,26 +62,24 @@ def write_azure_identity_binding(identityList):
         logger.debug(f'processing msi #{identity_id} containing')
 
         # Convert the data in the dictionary item into variables
-        filename = identity_info['msi'] + '.yaml'
+        filename = './output/' + identity_info['msi'] + '-binding.yaml'
         identityName = identity_info['msi']
-        resourceId = identity_info['Resource ID']
-        clientId = identity_info['Client ID']
 
         #Create a single YAML for the AzureIdentityBinding for each item in identityList
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
         with open (filename, 'w') as identityfile:
             logger.debug(f'processing file:{filename}')
             identityfile.write(f'apiVersion: "aadpodidentity.k8s.io/v1"\n')
-            identityfile.write(f'kind: AzureIdentity\n')
+            identityfile.write(f'kind: AzureIdentityBinding\n')
             identityfile.write(f'metadata:\n')
-            identityfile.write(f'  name: {identityName}\n')
+            identityfile.write(f'  name: {identityName}-binding\n')
             identityfile.write(f'spec:\n')
-            identityfile.write(f'  type: 0\n')
-            identityfile.write(f'  resourceID: {resourceId}\n')
-            identityfile.write(f'  clientID: {clientId}')
+            identityfile.write(f'  azureIdentity: {identityName}\n')
+            identityfile.write(f'  selector: {identityName}')
             identityfile.close()
-
 
 #read_input("single-line.csv")
 read_input_csv("example-identities.csv")
 write_azure_identity(msilist)
+write_azure_identity_binding(msilist)
 
